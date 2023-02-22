049bedde173134f3c427742f8c3c72823dc9da86
ayowilfred95 <38457345+ayowilfred95@users.noreply.github.com>
2023-02-22 19:04:57 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,8 +10,8 @@ import {Link } from 'react-router-dom';
 const Contact = () =>{
     return (
         <div>
-            <h1 className='text-orange-600 font-bold text-4xl text-center'>Follow us on our social media page</h1>
- <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            <h1 className='text-orange-600 font-bold text-4xl py-10 text-center'>Follow us on our social media page</h1>
+ <div className="text-5xl flex justify-center gap-16 py-10 text-gray-600 dark:text-gray-400">
               <Link href="https://www.twitter.com/ayomidewilfred9" target="_blank" >
               <AiFillTwitterCircle /> 
                 </Link>
